6bea7dee8820db815b17c69817536ca57d7e081b
Abuzar Aslam <abuzaraslam96@gmail.com>
2018-03-18 23:31:24 +0500

    Initial commit

---
+++ app/src/main/java/au/com/realestate/hometime/MainContractor.java
@@ -0,0 +1,22 @@
+package au.com.realestate.hometime;
+
+/**
+ * Created by abuzar.aslam on 3/13/2018.
+ */
+
+public interface MainContractor {
+
+
+    interface MainView{
+
+
+
+    }
+
+    interface MainPresenter{
+
+
+    }
+
+
+}
---
+++ app/src/main/java/au/com/realestate/hometime/MainPresenter.java
@@ -0,0 +1,8 @@
+package au.com.realestate.hometime;
+
+/**
+ * Created by abuzar.aslam on 3/13/2018.
+ */
+
+public class MainPresenter {
+}
